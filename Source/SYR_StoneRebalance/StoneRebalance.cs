e48613df8d26ad927e678b994409c85e07496d2e
Syrchalis <41159904+Syrchalis@users.noreply.github.com>
2018-10-21 00:56:25 +0200

    Add files via upload

---
+++ Source/SYR_StoneRebalance/StoneRebalance.cs
@@ -0,0 +1,49 @@
+﻿using Harmony;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using UnityEngine;
+using Verse;
+using Verse.AI;
+
+namespace SYR_StoneRebalance
+{
+    public class StoneRebalanceCore : Mod
+    {
+        public static StoneRebalanceSettings settings;
+
+        public StoneRebalanceCore(ModContentPack content) : base(content)
+        {
+            settings = GetSettings<StoneRebalanceSettings>();
+            var harmony = HarmonyInstance.Create("Syrchalis.Rimworld.StoneRebalance");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+
+        public override string SettingsCategory() => "StoneRebalanceSettingsCategory".Translate();
+
+        public override void DoSettingsWindowContents(Rect inRect)
+        {
+            checked
+            {
+                Listing_Standard listing_Standard = new Listing_Standard();
+                listing_Standard.Begin(inRect);
+                //listing_Standard.CheckboxLabeled("SyrThrumkin_useUnsupportedHair".Translate(), ref SyrNagaSettings.useUnsupportedHair, ("SyrThrumkin_useUnsupportedHairTooltip".Translate()));
+                //listing_Standard.Gap(24f);
+                if (listing_Standard.ButtonText("SyrThrumkin_defaultSettings".Translate(), "SyrThrumkin_defaultSettingsTooltip".Translate()))
+                {
+                    //SyrNagaSettings.useUnsupportedHair = false;
+                }
+                listing_Standard.End();
+                settings.Write();
+            }
+        }
+        public override void WriteSettings()
+        {
+            base.WriteSettings();
+        }
+    }
+}
