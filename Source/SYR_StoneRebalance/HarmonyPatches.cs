c4d3e28808efdf37cddf6591375c11ff5fed7e31
Syrchalis <41159904+Syrchalis@users.noreply.github.com>
2020-03-09 08:21:26 +0100

    Add files via upload

--- Source/SYR_StoneRebalance/HarmonyPatches.cs
+++ Source/SYR_StoneRebalance/HarmonyPatches.cs
@@ -1,11 +1,35 @@
-﻿using System;
+﻿using HarmonyLib;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Reflection;
+using RimWorld;
+using Verse;
+using UnityEngine;
+using Verse.AI;
 
 namespace SYR_StoneRebalance
 {
-    class HarmonyPatches
+    [HarmonyPatch(typeof(RecipeDef), nameof(RecipeDef.WorkAmountTotal))]
+    public class WorkAmountTotalPatch
     {
+        [HarmonyPostfix]
+        public static void WorkAmountTotal_Postfix(ref float __result, RecipeDef __instance, ThingDef stuffDef)
+        {
+            if (__instance == StoneRebalanceDefOf.Make_StoneBlocksAny)
+            {
+                __result = 1500;
+            }
+        }
+    }
+
+    [DefOf]
+    public static class StoneRebalanceDefOf
+    {
+        static StoneRebalanceDefOf()
+        {
+        }
+        public static RecipeDef Make_StoneBlocksAny;
     }
 }
